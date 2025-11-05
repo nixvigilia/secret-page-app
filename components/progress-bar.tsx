331420e1318e51188e3a18aa8980edbacc471231
Nixoy <john.nicholai.vigilia@gmail.com>
2025-11-06 02:12:49 +0800

    fix top loader

--- components/progress-bar.tsx
+++ components/progress-bar.tsx
@@ -5,12 +5,13 @@ import NextTopLoader from "nextjs-toploader";
 export function ProgressBar() {
   return (
     <NextTopLoader
-      color="hsl(var(--primary))"
+      color="hsl(0 0% 20.5%)"
       height={2}
       showSpinner={false}
       easing="ease"
       speed={200}
-      shadow="0 0 10px hsl(var(--primary)),0 0 5px hsl(var(--primary))"
+      shadow="0 0 10px hsl(0 0% 20.5%),0 0 5px hsl(0 0% 20.5%)"
+      zIndex={1600}
     />
   );
 }
